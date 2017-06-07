4bac0faa17b0d19fccbb914f6e5a6e8632a6227f
anxhelamjacaj <anxmja12@htl-shkoder.com>
2017-06-07 11:03:09 +0200

    Probe.php
    Ich habe eine andere Template gefunden , und wir werden es verwenden. Aber es ist noch nicht fertig.

---
+++ SourceCode/probe.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="description" content=""/>
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<link rel="stylesheet" type="text/css" href="../_css/style.css" media="screen" />
+	<title>Bee Organised | Home</title>
+</head>
+
+<body>
+
+<div id="site-wrapper">
+
+	<div id="header">
+
+		<div id="top">
+
+			<div class="left" id="logo">
+				<a href="probe.php"><img src="../_bilder/logo.jpg" style="float:left;" /></a>
+				
+			</div>
+
+			<div class="left navigation" id="main-nav">
+
+				<!--<a href="probe.php" id="logo">Bee Organised</a>-->
+				
+
+				<div class="section-content">
+					<form method="post" action="">
+						<input type="text" class="text" size="28" /> &nbsp; <input type="submit" class="button" value="Submit" />
+					</form>
+				</div>
+
+
+				<div class="clearer">&nbsp;</div>
+
+			</div>
+
+			<div class="clearer">&nbsp;</div>
+
+		</div>
+
+		<div class="navigation" id="sub-nav">
+
+			<ul class="tabbed">
+					<li><a href="probe.php">Home</a></li>
+					<li><a href="register.php">Registrieren</a></li>
+					<li><a href="login.php">Einloggen</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+
+			<div class="clearer">&nbsp;</div>
+
+		</div>
+
+	</div>
+
+	<div id="splash">
+
+		<div class="col3 left">
+
+			<h2 class="label label-green">Ausgangssituation</h2>
+				<p>In Albanien gibt es viele Imker, die auch sehr viel Bienenst&ouml;cke
+				haben und es ist unm&ouml;glich f&uuml;r sie alle Bienenst&ouml;cke zu kontrollieren
+				Es braucht auch sehr viel Zeit. F&uuml;r den Imker ist es auch unm&ouml;glich, 
+				dass er im Winter die Bienenst&ouml;cke in abgelegenen Orten wie z.B. Oblike 
+				usw. st&auml;ndig zu besuchen.Aufgrund der geografischen Lage der Bienenst&ouml;cke ist es nicht oder nur schwer m&ouml;glich, 
+				diese zu &uuml;berwachen. Daher soll unseres Projekt "Bee Organised" Imkern erm&ouml;glichen, 
+				Daten (Gewicht, Temperatur) des Bienenstocks entfernt &uuml;ber eine Webplattform abzurufen.
+				Es gibt in Albanien kein derartiges oder &auml;hnliches System.</p>
+			
+
+		</div>
+
+		<div class="col3-mid left">
+
+			<h2 class="label label-green">Beschreibung der Idee</h2>
+				<p>Es soll eine Anwendung (Plattform) zur Verwaltung und Überwachung von Bienenstöcken erstellt werden. Diese soll 
+				selbstständig Daten von den verwalteten Bienenstöcken abfragen und interpretieren sowie den Gesundheitszustand und 
+				detaillierte Daten für den Anwender übersichtlich darstellen. </p>
+				<p>Jeder Bienenstock wird dazu mit einer entsprechenden Sensorik ausgestattet, welche die wesentlichen Daten erfassen 
+				und über eine Kommunikationsschnittstelle an die zentrale Plattform übertragen kann. Es soll möglich sein, mehrere 
+				Bienenstöcke unabhängig vom Standort von der zentralen Plattform zu verwalten. Die Abfrage eines Bienenstocks soll 
+				allerdings auch ohne zentrale Plattform möglich sein.<p>
+
+		</div>
+
+		<div class="col3 right">
+			<h2 class="label label-green">Projektteam</h2>
+				<ul>
+					<li> Erda Ymeri | Projektleiterin </li>
+					<li> Anxhela Mjacaj | Projektleiterin Stv. </li>
+					<li> Ersamir Zekaj | Projektmitarbeiter </li>
+					<li> Elidon Bala | Projektmitarbeiter </li>
+				</ul>
+
+		</div>
+
+
+
+		<div class="right" id="footer-right">
+
+			<p class="large"><a href="#">Blog</a> <span class="text-separator">|</span> <a href="#">Events</a> <span class="text-separator">|</span> <a href="#">About</a> <span class="text-separator">|</span> <a href="#">Archives</a> <span class="text-separator">|</span> <strong><a href="#">Join Us!</a></strong> <span class="text-separator">|</span> <a href="#top" class="quiet">Page Top &uarr;</a></p>
+
+		</div>
+
+		<div class="clearer">&nbsp;</div>
+
+	</div>
+
+</div>
+
+</body>
+</html>
